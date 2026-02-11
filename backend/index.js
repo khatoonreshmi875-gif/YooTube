c764b2c9b3c60735af20ae6e26566e7f740126d0
Reshmi <khatoonreshmi875@gmail.com>
2026-02-11 18:11:48 +0530

    add port

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,21 @@
+// import dotenv from "dotenv";
+
+// import connectDB from "./src/db/index.js";
+
+// import app from "./src/app.js";
+
+// dotenv.config({
+//   path: "./.env",
+// });
+
+// connectDB()
+//   .then(() => {
+//     console.log("MongoDB connected successfully");
+//   })
+//   .catch((err) => {
+//     console.log("MongoDB connection failed!!", err);
+//   });
+// // export default app;
 import dotenv from "dotenv";
 
 import connectDB from "./src/db/index.js";
@@ -10,9 +28,13 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    console.log("MongoDB connected successfully");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is  running at port ${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.error("Something is  wrong ", error);
+    });
   })
   .catch((err) => {
     console.log("MongoDB connection failed!!", err);
   });
-export default app;
