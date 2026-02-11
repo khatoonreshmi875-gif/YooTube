04fd0343c1f5107a3a7a0393f6e3d3ff61ac547f
Reshmi <khatoonreshmi875@gmail.com>
2026-02-11 18:17:50 +0530

    port remove

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,3 @@
-// import dotenv from "dotenv";
-
-// import connectDB from "./src/db/index.js";
-
-// import app from "./src/app.js";
-
-// dotenv.config({
-//   path: "./.env",
-// });
-
-// connectDB()
-//   .then(() => {
-//     console.log("MongoDB connected successfully");
-//   })
-//   .catch((err) => {
-//     console.log("MongoDB connection failed!!", err);
-//   });
-// // export default app;
 import dotenv from "dotenv";
 
 import connectDB from "./src/db/index.js";
@@ -28,13 +10,31 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is  running at port ${process.env.PORT}`);
-    });
-    app.on("error", (error) => {
-      console.error("Something is  wrong ", error);
-    });
+    console.log("MongoDB connected successfully");
   })
   .catch((err) => {
     console.log("MongoDB connection failed!!", err);
   });
+export default app;
+// import dotenv from "dotenv";
+
+// import connectDB from "./src/db/index.js";
+
+// import app from "./src/app.js";
+
+// dotenv.config({
+//   path: "./.env",
+// });
+
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is  running at port ${process.env.PORT}`);
+//     });
+//     app.on("error", (error) => {
+//       console.error("Something is  wrong ", error);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("MongoDB connection failed!!", err);
+//   });
