8414021d757676ccaafc073476875f99c9f322c6
Reshmi <khatoonreshmi875@gmail.com>
2026-02-12 00:59:49 +0530

    fix(auth): return 200 status on successful password reset instead of 400

--- backend/src/controllers/user.controller/auth/password/ResetPassword.js
+++ backend/src/controllers/user.controller/auth/password/ResetPassword.js
@@ -3,12 +3,10 @@ import asynchandler from "../../../../utils/asynchandler.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { ApiResponse } from "../../../../utils/ApiResponse.js";
-import ApiError from "../../../../utils/ApiError.js";
 
 export const resetPassword = asynchandler(async (req, res) => {
   const { token } = req.query;
   const { password, confirmPassword } = req.body;
-  console.log("run reset password", token, password, confirmPassword);
   if (password !== confirmPassword) {
     return res
       .status(400)
@@ -19,12 +17,10 @@ export const resetPassword = asynchandler(async (req, res) => {
   const user = await User.findById(decodedToken._id);
   const Oldpassword = user.password;
   user.password = password;
-  console.log("reset-password", Oldpassword, user.password);
 
   await user.save();
-  console.log(user.password, Oldpassword, user, password);
-  console.log("updation", user);
+  
   res
-    .status(400)
-    .json(new ApiResponse(400, [user], "password Updated successfully "));
+    .status(200)
+    .json(new ApiResponse(200, [user], "password Updated successfully "));
 });
